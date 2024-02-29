29059dcf572a7c40975d176d1b751d70b8b097c2
SAMAHA <elio.samaha@etu.sorbonne-universite.fr>
2024-02-29 18:11:05 +0100

    done

--- reduced_ghc.py
+++ reduced_ghc.py
@@ -8,28 +8,55 @@ from ghc import *
 
 # Symboles productifs
 # -------------------
-
+        
 def prod0(t,r):
     # t : symboles terminaux
     # r : liste de productions
-    # A COMPLETER
-    return
+    res = []
+    for elem, prod in r:
+        for der in prod:
+            vrai = 1
+            for i in der:
+                if not is_in(eq_atom , i , t) :
+                    vrai = 0
+            if vrai == 1:
+                res = ajout(eq_atom, elem, res)
+ 
+    return res
 
 def next_prod(t,r,eqnt,prev):
     # t : symboles terminaux
     # r : liste de productions
     # eqnt : egalite sur les non terminaux
     # prev : liste de non terminaux de depart
-    # A COMPLETER
-    return
+    res = prev.copy()
+    for elem, rp in r:
+        for der in rp:
+            vrai = 1
+            for i in der:
+                if not is_in(eqnt , i , t) and not is_in(eqnt , i , prev):
+                    vrai = 0
+            if vrai == 1:
+                res = ajout(eq_atom, elem, res)
+ 
+    return res
 
 
 def prod(t,r,eqnt):
     # t : symboles terminaux
     # r : liste de productions
     # eqnt : egalite sur les non terminaux
-    # A COMPLETER
-    return
+    nt=[n for n,_ in r]
+    eq_prod = make_eq_prod(nt, eqnt)
+    prev = prod0(t,r)
+    curr = next_prod(t,r,eqnt, prev)
+
+    while not eq_prod(prev, curr):
+        
+        prev = curr
+        curr = next_prod(t,r,eqnt, prev)
+        
+    return curr
 
 # Elimination des symboles non productifs d'une GHC
 # -------------------------------------------------
@@ -59,8 +86,8 @@ def remove_nt(g,ent):
 
 def remove_non_prod(g):
     # g : ghc
-    # A COMPLETER
-    return
+    (g_nt,g_t,g_r,g_s,eq_atom) = g
+    return remove_nt(g, prod(g_t,g_r,eq_atom))
 
 # Symboles accessibles
 # --------------------
@@ -70,29 +97,43 @@ def next_reach(nt,r,eqnt,prev):
     # r : liste de productions
     # eqnt : egalite sur les non terminaux
     # prev : liste de non terminaux de depart
-    # A COMPLETER
-    return
+    
+    res = []
+
+    for s in prev :
+        for (si,li) in r :
+            if eqnt(s,si):
+                for k in li :
+                    for j in k :
+                        if is_in(eqnt,j,nt) :
+                            res = ajout(eqnt, j,res)
+    return union(eqnt,res,prev)
+
+def reach_aux(e):
+    (nt , r , eqnt , prev) = e
+    return (nt , r , eqnt , next_reach(nt , r , eqnt , prev))
 
 def reach(nt,r,si,eqnt):
     # nt : symboles non terminaux
     # r : liste de productions
     # si : symbole de depart
     # eqnt : egalite sur les non terminaux
-    # A COMPLETER
-    return
+
+    return fixpoint_from(make_eq_set(eqnt) , reach_aux , (nt , r , eqnt , [si]))[-1]
+
 
 # Elimination des symboles non accessibles d'une GHC
 # -------------------------------------------------
 
 def remove_non_reach(g):
     # g : ghc
-    # A COMPLETER
-    return
+    (g_nt,g_t,g_r,g_s,eq_atom) = g
+    return remove_nt(g, reach(g_nt,g_r,g_s,eq_atom))
 
 # Reduction d'une grammaire hors-contexte
 # ---------------------------------------
 
 def reduce_grammar(g):
     # g : ghc
-    # A COMPLETER
-    return
+    
+    return remove_non_reach(remove_non_prod(g))
